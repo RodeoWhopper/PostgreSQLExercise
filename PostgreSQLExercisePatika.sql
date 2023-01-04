28a6f085649d0a53ed026773d8ba75d47996a193
Emirhan Bulut <emirhanblt9@gmail.com>
2023-01-04 12:48:43 +0300

    5. Homework Attempt

--- PostgreSQLExercisePatika.sql
+++ PostgreSQLExercisePatika.sql
@@ -34,7 +34,7 @@ SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 SELECT first_name, last_name FROM actor WHERE first_name IN ('Penelope','Nick','Ed');
 
 --3
-SELECT * FROM film WHERE rental_rate IN (0.99,2.99,4.99) AND replacement_cost (12.99,15.99,28.99);
+SELECT * FROM film WHERE rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99);
 
 -- ODEV 3 --
 /*
@@ -77,6 +77,31 @@ SELECT COUNT(*) FROM country WHERE country LIKE '_____';
 
 --5
 SELECT COUNT(*) FROM city WHERE city ILIKE '%r';  
+
+-- ODEV 5 --
+/*
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+*/
+--1
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+--2
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+--3
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
 
 
 
