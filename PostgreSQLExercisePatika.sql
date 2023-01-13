6394c60f749c0468c930e28dec51522edc9fb47b
Emirhan Bulut <emirhanblt9@gmail.com>
2023-01-13 13:31:55 +0300

    9. Homework Attempt

--- PostgreSQLExercisePatika.sql
+++ PostgreSQLExercisePatika.sql
@@ -259,5 +259,22 @@ WHERE id = 31;
 DELETE employee
 WHERE id = 16;
 
+-- ODEV 9 --
+/*
+city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+*/
+--1
+SELECT city.city, country.country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+--2
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id;
+
+--3
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id;
 
 
