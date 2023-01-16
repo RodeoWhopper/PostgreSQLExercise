aaaf8d0fc4d862fc28c8af5089414462fef0bf92
Emirhan Bulut <emirhanblt9@gmail.com>
2023-01-16 15:55:57 +0300

    11. Homework Attempt

--- PostgreSQLExercisePatika.sql
+++ PostgreSQLExercisePatika.sql
@@ -295,5 +295,41 @@ RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
 SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
 FULL JOIN rental ON customer.customer_id = rental.customer_id;
 
+-- ODEV 11 --
+/*
+actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+İlk 3 sorguyu tekrar eden veriler için de yapalım.
+*/
+--1
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer;
 
+--2
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer;
 
+--3
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
+
+--4
+
+---1
+SELECT first_name FROM actor
+UNION ALL
+SELECT first_name FROM customer;
+
+---2
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer;
+
+---3
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer;
